99857f8d842594a0a98457d0d497aa9fb2e4edf8
Ben Warren <warrenbhw@gmail.com>
2024-11-27 08:54:06 -0800

    build succeeds

--- src/include/notion_requests.hpp
+++ src/include/notion_requests.hpp
@@ -21,6 +21,7 @@ namespace duckdb
     std::string perform_https_request(const std::string &host, const std::string &path, const std::string &token,
                                       HttpMethod method, const std::string &body, const std::string &content_type);
     std::string call_notion_api(const std::string &token, HttpMethod method, const std::string &path, const std::string &body);
+    std::string list_databases(const std::string &token);
     std::string get_database(const std::string &token, const std::string &database_id);
     std::string query_database(const std::string &token, const std::string &database_id);
     std::string create_page(const std::string &token, const std::string &database_id, const std::string &body);
--- test/notion_test.cpp
+++ test/notion_test.cpp
@@ -1,5 +1,4 @@
-#define CATCH_CONFIG_MAIN
-#include <catch2/catch.hpp>
+#include <catch2/catch_test_macros.hpp>
 #include "notion_requests.hpp"
 #include "notion_utils.hpp"
 #include "duckdb/common/types/value.hpp"
