3a0dfe288ca5401b43bdf154c52ea1b24e0126b7
Ben Warren <warrenbhw@gmail.com>
2024-11-30 14:08:39 -0800

    remove interface from header

--- src/include/notion_requests.hpp
+++ src/include/notion_requests.hpp
@@ -15,8 +15,6 @@ namespace duckdb
         DELETE
     };
 
-    std::string perform_https_request(const std::string &host, const std::string &path, const std::string &token,
-                                      HttpMethod method, const std::string &body, const std::string &content_type);
     std::string call_notion_api(const std::string &token, HttpMethod method, const std::string &path, const std::string &body);
     std::string get_database(const std::string &token, const std::string &database_id);
     std::string query_database(const std::string &token, const std::string &database_id);
